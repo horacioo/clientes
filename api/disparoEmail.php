e727c62538ed75864d4138af13ba4b0b8dcdedba
Horácio J <lanterna_@hotmail.com>
2018-01-25 11:12:35 -0200

    quinta feira

---
+++ api/disparoEmail.php
@@ -0,0 +1,21 @@
+<?php
+
+////////////////////////////////////////////////require_once '../../../../wp-config.php';
+
+
+/*
+require_once '../../../../wp-config.php';
+require_once '../include/Emails.php';
+use emailsProcessosEDados\Emails as em;
+$x = em::EnvioDeEmails();
+print_r($x);
+sleep(2);
+*/
+
+/*
+            $to      = "lanterna_@hotmail.com"; //"lanterna_@hotmail.com";
+            $subject = "email das ".date("d/m/Y H:i:s");
+            $content = "só testando o cron";
+            $headers = array('Content-Type: text/html; charset=UTF-8');
+            $status  = wp_mail($to, $subject, $content, $headers);
+            */
--- include/Emails.php
+++ include/Emails.php
@@ -2,7 +2,8 @@
 
 namespace emailsProcessosEDados;
 
-class Emails {
+class Emails
+    {
 
     public static $extension = "";
     public static $texto;
@@ -22,23 +23,52 @@ class Emails {
 
 
 
-
-
     static private function pegaTexto($id) {
         global $wpdb;
         self::$extension = $wpdb->prefix;
         $query           = "select * from  " . self::$extension . "posts where id=$id";
         $dados           = $wpdb->get_results($query, ARRAY_A);
-        self::$texto[]   = array("conteudo"=>$dados[0]['post_content'],"titulo"=>$dados[0]['post_title']);
+        self::$texto[]   = array("conteudo" => $dados[0]['post_content'], "titulo" => $dados[0]['post_title']);
     }
 
 
 
 
 
+    static public function EnvioDeEmails() {
+        return self::Aniversario();
+    }
 
 
 
 
 
-}
+    private static function Aniversario() {
+        global $wpdb;
+        $Sel   = "SELECT * FROM `clientes` WHERE day(dataNascimento) = '" . date("d") . "' and month(dataNascimento)='" . date("m") . "'";
+        $Sel   = "SELECT cli.nome, e.email 
+                  FROM `clientes` as cli
+                  inner join clientesemail as cle on cle.clientes = cli.id
+                  inner join email as e on e.id = cle.email
+                  WHERE day(cli.dataNascimento) = '" . date("d") . "' and month(cli.dataNascimento)='" . date("m") . "'";
+        $dados = $wpdb->get_results($Sel, ARRAY_A);
+        foreach ($dados as $d):
+                   echo "<hr> ".$d['nome']." ||";
+                   echo "  ".$d['email']."<hr>";
+                   //envia email
+                   //os que forem enviados,salvo informação em tabela com o tipo de email no caso, de aniversário, é 1
+                   //próximo
+        endforeach;
+    }
+    
+    
+    private static function Email(){}
+    private static function salvaEmTabela(){}
+
+          
+
+
+
+
+
+    }
